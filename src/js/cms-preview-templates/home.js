a31c655030385c67a69e4cfb02668537b320a2a7
Pierre <pierre.plantie@gmail.com>
2018-04-20 01:57:58 +0100

    Update home.js

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -52,7 +52,7 @@ export default class PostPreview extends React.Component {
               </div>
 
               <div className="w-60-l ph2-l">
-                <img src="/img/home-about-section.jpg" alt="" className="mb3"/>
+                <img src={getAsset(entry.getIn(['data', 'full_image']))} alt="" className="db w-100"/>
               </div>
             </div>
 
